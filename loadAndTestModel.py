57503fc293fbf7fbff54dc6d6dcaa599bf1d2c78
Axel Boell <axelboell@web.de>
2022-05-31 20:21:26 +0200

    Add Some Statistics for testing Model

--- loadAndTestModel.py
+++ loadAndTestModel.py
@@ -23,26 +23,34 @@ def eazy_env():
     new_logger = configure(tmp_path, ["tensorboard", "stdout"])
     # model = PPO.load("./tmp/myModel", env=env)
     time.sleep(0.3)
-    model = PPO.load("./tmp/crash01.zip", env=env)
+    model = PPO.load("./tmp/Callback59.7322", env=env)
     # model.set_random_seed(123)
     time.sleep(0.3)
     obs = env.reset()
     rewards = 0
-    for i in range(10):
+    auswertung = []
+    for i in range(30):
         for _ in range(env.max_tick_count):
             action, _states = model.predict(obs, deterministic=True)
             obs, reward, done, info = env.step(action)
             rewards += reward
             # env.render()
-            if i > 0:
-                sleep(0.01)
+            # if i > 0:
+            #     sleep(0.01)
             if done:
                 print("Reward:", rewards)
+                auswertung.append(rewards)
                 rewards = 0
                 env.reset()
                 break
 
     obs = env.close()
+
+    import statistics
+    mean = statistics.mean(auswertung)
+    max_v = max(auswertung)
+    min_v = min(auswertung)
+    print('max:', max_v, 'min:', min_v, 'mean', mean, 'max-min:',  max_v-min_v)
     #
     # mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
     # print(mean_reward, std_reward)
